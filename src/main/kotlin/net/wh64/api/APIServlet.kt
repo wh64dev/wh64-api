041d0cf5ce87aa80d765f76823a26a33dc2b0e7c
WH64 <me@projecttl.net>
2024-04-22 23:20:33 +0900

    feat: check health

--- src/main/kotlin/net/wh64/api/APIServlet.kt
+++ src/main/kotlin/net/wh64/api/APIServlet.kt
@@ -5,15 +5,25 @@ import jakarta.servlet.annotation.WebServlet
 import jakarta.servlet.http.HttpServlet
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import kotlinx.coroutines.runBlocking
 import net.wh64.api.model.check.HealthCheck
+import net.wh64.api.util.DatabaseUtil
+import org.jetbrains.exposed.sql.SchemaUtils
 
 @WebServlet(name = "api", value = ["/v1", "/v1/"])
 class APIServlet : HttpServlet() {
+    private val database = DatabaseUtil()
+
     override fun doGet(req: HttpServletRequest, res: HttpServletResponse) {
         val start = System.currentTimeMillis()
         res.contentType = "application/json"
         res.status = HttpServletResponse.SC_OK
         res.characterEncoding = Charsets.UTF_8.name()
+        runBlocking {
+            database.dbQuery {
+                return@dbQuery SchemaUtils.listTables()
+            }
+        }
 
         val gson = Gson()
         res.writer.use { out ->
